1a4a12d23044a05d5f42605a3c4f1fef18d0f301
lollinng <pratameshjadhav@gmail.com>
2023-09-19 16:52:59 +0530

    Added admin view for db tables

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Category, Product
+
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    # here slug which is url_name for category is created randomly from the name
+    prepopulated_fields = {'slug':('name',)}
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','author','slug','price','in_stock','created','updated']
+    list_filter = ['in_stock','is_active']
+    list_editable = ['price','in_stock']
+    prepopulated_fields = {'slug':('title',)}
+
+    
