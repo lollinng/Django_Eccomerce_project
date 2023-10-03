1935fd34ecc8f5397131b2724a207a9f4bcc7026
lollinng <pratameshjadhav@gmail.com>
2023-10-03 12:33:01 +0530

    Added login and logout functionalities

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm,
+                                       SetPasswordForm)
 
 from .models import UserBase
 
@@ -48,3 +50,21 @@ class RegistrationForm(forms.ModelForm):
             {'class': 'form-control mb-3', 'placeholder': 'Password'})
         self.fields['password2'].widget.attrs.update(
             {'class': 'form-control', 'placeholder': 'Repeat Password'})
+        
+
+# OVERRIDE THE AUthentication form to add bootstrap component
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password',
+                'id': 'login-pwd',
+            }
+        )
+    )
--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
+
 from . import views
+from .forms import (UserLoginForm)
 
 
 app_name = 'account'
@@ -8,4 +12,6 @@ urlpatterns = [
     path('register/',views.account_register,name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
     path('dashboard/',views.dashboard,name='dashboard'),
+    path('login/',auth_views.LoginView.as_view(template_name='account/registration/login.html',form_class=UserLoginForm),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(next_page='/account/login/'),name='logout')
 ]
