b6335ff8125fc81c526b4b68bd2586c0d5e91e90
lollinng <pratameshjadhav@gmail.com>
2023-10-03 16:24:15 +0530

    Added Password reset functionality for the user

--- account/forms.py
+++ account/forms.py
@@ -89,3 +89,26 @@ class UserEditForm(forms.ModelForm):
         super().__init__(*args,**kwargs)
         self.fields['first_name'].required = True
         self.fields['email'].required = True
+
+# overriding the in-built form PasswordResetForm for adding bootstrap elements and one validation
+class PwdResetForm(PasswordResetForm):
+    
+    email = forms.EmailField(max_length=254, widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        u = UserBase.objects.filter(email=email)
+        if not u:
+            raise forms.ValidationError(
+                'Unfortunately we can not find that email address')
+        return email
+
+# Overriding SetPasswordForm with 2 char fields with bootstrap
+class PwdResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label='New password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
+    new_password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
